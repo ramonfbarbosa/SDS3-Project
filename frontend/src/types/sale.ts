a29f0d1a2941f95fb0fb631df1a27442006f41f6
Ramon Fernandes <ramonmfb777@gmail.com>
2021-05-09 16:42:27 -0300

    DataTable integracao/paginacao

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,24 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+export type SalePage = {
+    content?: Sale[];
+    totalPages: number;
+    totalElements: number;
+    last: boolean;
+    size?: number;
+    number: number;
+    numberOfElements?: number;
+    first: boolean;
+    empty?: boolean;
+}
 export type SaleSum = {
     sellerName: string,
     sum: number
